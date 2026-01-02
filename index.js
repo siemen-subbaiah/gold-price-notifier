3f801cea5c89a13804a9315d4f1f75ca07b9106f
siemen_subbaiah <siemen.s@iqnext.io>
2026-01-02 19:08:00 +0530

    modified yml file for smooth execution of puppeteer

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ async function sendTelegramMessage(message) {
 async function getGoldPrices() {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ],
   });
   const page = await browser.newPage();
 
